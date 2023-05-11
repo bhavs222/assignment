1f44b78685d4d4cf40fac1325d8b66d62a879299
bhavs222 <parnasalabhavana222@gmail.com>
2023-05-11 18:24:23 +0530

    updated

--- assignment.py
+++ assignment.py
@@ -9,4 +9,15 @@ if attendance == 1:
 else:
     print("Employee is Absent")
 
+#pgm to daily employee wage
+import random
+hourly_rate = 20  
+min_hours_worked = 1  
+max_hours_worked = 8  
+hours_worked = random.randint(min_hours_worked, max_hours_worked)
+daily_wage = hourly_rate * hours_worked
+print("Hours worked:", hours_worked)
+print("Daily wage of employee:", daily_wage)
+
+
 
