e419e82482fd825556a78076c2649a68af961eda
bhavs222 <parnasalabhavana222@gmail.com>
2023-05-12 12:24:01 +0530

    n

--- assignment.py
+++ assignment.py
@@ -4,11 +4,11 @@ print("Welcome to EmployeeWage Computation Program on Main Branch")
 #pgm to absent or present 
 import random
 attendance = random.randint(0, 1)
-if attendance == 1:
+if attendance == 100:
     print("Employee is Present")
 else:
     print("Employee is Absent")
-
+bhhjjkkkk
 #pgm to daily employee wage
 import random
 hourly_rate = 20  
@@ -18,8 +18,8 @@ hours_worked = random.randint(min_hours_worked, max_hours_worked)
 daily_wage = hourly_rate * hours_worked
 print("Hours worked:", hours_worked)
 print("Daily wage of employee:", daily_wage)
-
-
+mkmkkm;s
+ nmjkjjjjj
 # print
 print("hi")
 #Add Part timeEmployee & Wage
